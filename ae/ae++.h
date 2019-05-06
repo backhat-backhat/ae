086b5dd21b714aae7c4bfba509db0e41c2fd24ea
xx <>
2019-05-06 11:38:25 +0800

    first commit

---
+++ ae/ae++.h
@@ -0,0 +1,15 @@
+#ifndef __AE++_H__
+#define __AE++_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+    
+#include "ae.h"
+#include "zmalloc.h"
+    
+#ifdef __cplusplus
+}
+#endif
+
+#endif // __AE++_H__
